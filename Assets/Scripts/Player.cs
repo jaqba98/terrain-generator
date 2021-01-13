04202e218bf05c0b2aa4ab996cfbef13598b6ee8
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2021-01-13 21:07:16 +0100

    Add functionality to generate empty object in chunk position when player discover chunk

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,19 @@ public class Player : MonoBehaviour
 {
     private Vector3 lastPosition;
 
-    private void Start()
-    {
-        lastPosition = transform.position;
-    }
-
     public bool IsNotMoving()
     {
         if (transform.position == lastPosition) return true; 
         lastPosition = transform.position;
         return false;
+    }
+
+    public Vector3Int GetVector3IntPosition(int size)
+    {
+        return new Vector3Int(
+            (int) transform.position.x / size * size,
+            (int) transform.position.y / size * size,
+            (int) transform.position.z / size * size
+        );
     }
 }
--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -2,14 +2,34 @@ using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
+    public int size;
+
     private Player player;
 
-    private void Start() => player = FindObjectOfType<Player>();
+    private void Start()
+    {
+        Init();
+        GenerateTerrain();
+    }
 
-    private void Update()
+    private void Init()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    private void FixedUpdate()
     {
         if (player.IsNotMoving()) return;
 
-        Debug.Log("IsMoving");
+        GenerateTerrain();
+    }
+
+    private void GenerateTerrain()
+    {
+        Vector3Int position = player.GetVector3IntPosition(size);
+
+        if (GameObject.Find(position.ToString())) return;
+
+        new GameObject(position.ToString());
     }
 }
