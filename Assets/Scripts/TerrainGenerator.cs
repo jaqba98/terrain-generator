2319c4e072cd86760273e19c6af532213fe5403d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2021-01-13 22:14:58 +0100

    Terrain generator generate infinite flat world

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -6,6 +6,8 @@ public class TerrainGenerator : MonoBehaviour
 
     private Player player;
 
+    public GameObject plane;
+
     private void Start()
     {
         Init();
@@ -28,8 +30,26 @@ public class TerrainGenerator : MonoBehaviour
     {
         Vector3Int position = player.GetVector3IntPosition(size);
 
+        CreateTerrain(position);
+        CreateTerrain(new Vector3Int(position.x + size, position.y, position.z));
+        CreateTerrain(new Vector3Int(position.x - size, position.y, position.z));
+        CreateTerrain(new Vector3Int(position.x, position.y, position.z + size));
+        CreateTerrain(new Vector3Int(position.x, position.y, position.z - size));
+        CreateTerrain(new Vector3Int(position.x + size, position.y, position.z + size));
+        CreateTerrain(new Vector3Int(position.x + size, position.y, position.z - size));
+        CreateTerrain(new Vector3Int(position.x - size, position.y, position.z + size));
+        CreateTerrain(new Vector3Int(position.x - size, position.y, position.z - size));
+    }
+
+    private void CreateTerrain(Vector3Int position)
+    {
         if (GameObject.Find(position.ToString())) return;
 
-        new GameObject(position.ToString());
+        GameObject chunk = new GameObject(position.ToString());
+        chunk.transform.position = position;
+        GameObject terrain = Instantiate(plane, position, chunk.transform.rotation);
+        terrain.transform.parent = chunk.transform;
+
+        chunk.transform.parent = transform;
     }
 }
